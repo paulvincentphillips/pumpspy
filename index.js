9e1da72ca212443c545fea63c39ed60b69e39f27
Paul Phillips <paulvincentphillips@gmail.com>
2021-09-19 23:25:22 +0100

    Add fix Heroku database 503 error

--- index.js
+++ index.js
@@ -12,13 +12,11 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "client/build")));
-
-// if (process.env.NODE_ENV === "production") {
-//   //server static content
-//   //npm run build
-//   app.use(express.static(path.join(__dirname, "client/build")));
-// }
+if (process.env.NODE_ENV === "production") {
+  //server static content
+  //npm run build
+  app.use(express.static(path.join(__dirname, "client/build")));
+}
 
 //ROUTES//
 

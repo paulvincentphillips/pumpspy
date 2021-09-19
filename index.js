73c6635c4b6a2819a4792335e8db5185b2eba301
Paul Phillips <paulvincentphillips@gmail.com>
2021-09-19 22:56:32 +0100

    Fix petrol price update route

--- index.js
+++ index.js
@@ -12,11 +12,13 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-if (process.env.NODE_ENV === "production") {
-  //server static content
-  //npm run build
-  app.use(express.static(path.join(__dirname, "client/build")));
-}
+app.use(express.static(path.join(__dirname, "client/build")));
+
+// if (process.env.NODE_ENV === "production") {
+//   //server static content
+//   //npm run build
+//   app.use(express.static(path.join(__dirname, "client/build")));
+// }
 
 //ROUTES//
 
@@ -36,7 +38,7 @@ app.put("/stations/petrol/:id", async (req, res) => {
     const { id } = req.params;
     const { price } = req.body;
     await pool.query(
-      "UPDATE stations SET updated = CURRENT_TIMESTAMP WHERE station_id = $2",
+      "UPDATE stations SET petrol = $1, updated = CURRENT_TIMESTAMP WHERE station_id = $2",
       [price, id]
     );
     res.json("Station's Petrol price has been updated!");

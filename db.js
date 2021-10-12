6acdfc6c4764c4ebfe7ad302bacfb4985866f866
Paul Phillips <paulvincentphillips@gmail.com>
2021-10-12 18:41:53 +0100

    Server changes

--- db.js
+++ db.js
@@ -1,16 +1,6 @@
-const Pool = require("pg").Pool;
-require("dotenv").config();
+const { Pool } = require('pg')
 
-const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
-
-const proConfig = process.env.DATABASE_URL; //heroku addons
-
-const pool = new Pool({
-  connectionString:
-    process.env.NODE_ENV === "production" ? proConfig : devConfig,
-  // ssl: {
-  //   rejectUnauthorized: false,
-  // },
-});
-
-module.exports = pool;
+const pool = new Pool()
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+}
--- index.js
+++ index.js
@@ -2,16 +2,11 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const path = require("path");
-//const PORT = process.env.PORT || 5000;
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
 app.use(express.json());
-
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "client/build")));
-// }
 
 //ROUTES//
 
